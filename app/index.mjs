e3282102e15d7992138002c60bf2a67a31635ce7
365cent <74791570+365cent@users.noreply.github.com>
2024-06-07 23:32:31 +0800

    debug: allow any domain

--- app/index.mjs
+++ app/index.mjs
@@ -3,6 +3,7 @@ import * as bare from "../bareServer/bs.index.mjs";
 
 const httpServer = http.createServer((req, res) => {
   res.setHeader('Cache-Control', 'public, max-age=3600');
+  res.setHeader('Access-Control-Allow-Origin', '*');
 });
 
 httpServer.on('request', bare.sourceRequest);
